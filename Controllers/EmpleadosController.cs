797aaa0076dfc792816da20a543812c98e67449a
adrinetor86 <adrinetor81@gmail.com>
2026-03-25 13:48:11 +0100

    Add project files.

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiEmpleados.Services;
+using NugetApiModels.Models;
+
+namespace MvcCoreApiEmpleados.Controllers;
+
+public class EmpleadosController : Controller
+{
+    private ServiceEmpleados _serviceEmpleados;
+
+    public EmpleadosController(ServiceEmpleados service)
+    {
+        _serviceEmpleados = service;
+    }
+    
+    
+    public async Task<IActionResult> Index()
+    {
+        List<string> oficios = await _serviceEmpleados.GetOficiosAsync();
+        List<Empleado> empleados= await _serviceEmpleados.GetEmpleadosAsync();
+        ViewData["OFICIOS"] = oficios;
+        return View(empleados);
+    }
+}
---
+++ Services/ServiceEmpleados.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http.Headers;
+using NugetApiModels.Models;
+
+namespace MvcCoreApiEmpleados.Services;
+
+public class ServiceEmpleados
+{
+    private string _ApiUrl;
+
+    private MediaTypeWithQualityHeaderValue _header;
+
+    public ServiceEmpleados(IConfiguration configuration)
+    {
+        _ApiUrl = configuration.GetValue<string>("ApiUrls:ApiEmpleados");
+        _header = new MediaTypeWithQualityHeaderValue("application/json");
+    }
+    public async Task<List<Empleado>> GetEmpleadosAsync()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string request = "api/Empleados";
+            client.BaseAddress = new Uri(_ApiUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(_header);
+            HttpResponseMessage response= await client.GetAsync(request);
+
+            if (response.IsSuccessStatusCode == true)
+            {
+                List<Empleado> data= await response.Content.ReadAsAsync<List<Empleado>>();
+                return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }  
+    
+    public async Task<List<string>> GetOficiosAsync()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string request = "api/Empleados/Oficios";
+            client.BaseAddress = new Uri(_ApiUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(_header);
+            HttpResponseMessage response= await client.GetAsync(request);
+
+            if (response.IsSuccessStatusCode == true)
+            {
+                List<string> data= await response.Content.ReadAsAsync<List<string>>();
+                return data;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    
+    
+}
